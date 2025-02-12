9e8c845a348733f7139a4e2585bb9decd96a5a48
Syed Huzaif <syedhuzaifalishah987@gmail.com>
2025-02-12 15:32:19 +0500

    Update Problem 05.cpp

--- OOP Lab/Lab 02/Problem 05.cpp
+++ OOP Lab/Lab 02/Problem 05.cpp
@@ -1,86 +1,55 @@
 /*
 * Name: Huzaif Ali
 * Class: BCS-2C
-* Question: 04
- */
+* Question: 05
+*/
 
 /*
-* Create a program to manage a dynamic array of strings. The program should: 
-* 1. Allow the user to input the number of strings. 
-* 2. Dynamically allocate memory for the strings. 
-* 3. Populate the array with user input. 
-* 4. Sort the strings alphabetically and display them. 
-* 5. Deallocate memory after use. 
+* Write a program to demonstrate the relationship between pointers and arrays. The program should: 
+* 1. Allow the user to input an array of integers. 
+* 2. Use pointer arithmetic to traverse and display the array. 
+* 3. Find the sum of the array elements using pointers. 
+* 4. Display the results and deallocate memory. 
 */
 
 #include <iostream>
+
 using namespace std;
 
-void inputStrings(char** arr, int numStrings) {
-    for (int i = 0; i < numStrings; ++i) {
-        cout << "Enter string " << i + 1 << ": ";
-        cin >> arr[i];
-    }
-}
-
-
-void swapStrings(char*& str1, char*& str2) {
-    char* temp = str1;
-    str1 = str2;
-    str2 = temp;
-}
-
-
-void sortStrings(char** arr, int numStrings) {
-    for (int i = 0; i < numStrings - 1; ++i) {
-        for (int j = 0; j < numStrings - i - 1; ++j) {
-            int k = 0;
-            while (arr[j][k] != '\0' && arr[j + 1][k] != '\0' && arr[j][k] == arr[j + 1][k]) {
-                k++;
-            }
-            if (arr[j][k] > arr[j + 1][k]) {
-                swapStrings(arr[j], arr[j + 1]);
-            }
-        }
-    }
-}
-
-
-void displayStrings(char** arr, int numStrings) {
-    cout << "\nSorted Strings:\n";
-    for (int i = 0; i < numStrings; ++i) {
-        cout << arr[i] << endl;
-    }
-}
-
-
 int main() {
-    int numStrings, maxLength;
+    int size;
 
-    // 1. Number of Strings
-    cout << "Enter the number of strings: ";
-    cin >> numStrings;
+    // 1. Number
+    cout << "Enter the number of elements: ";
+    cin >> size;
 
     // 2. Allocation
-    char** strings = new char*[numStrings];
-    for (int i = 0; i < numStrings; ++i) {
-        strings[i] = new char[100];
-    }
+    int* arr = new int[size];
 
     // 3. Input
-    inputStrings(strings, numStrings);
-
-    // 4. Sort
-    sortStrings(strings, numStrings);
-
-    // 5. Display
-    displayStrings(strings, numStrings);
-
-    // 6. Deallocation
-    for (int i = 0; i < numStrings; ++i) {
-        delete[] strings[i];
+    cout << "Enter " << size << " elements:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> *(arr + i);
     }
-    delete[] strings;
+
+    // 4. Display Inputs
+    cout << "Array elements: ";
+    for (int i = 0; i < size; ++i) {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+
+    // 5. Sum
+    int sum = 0;
+    for (int* ptr = arr; ptr < arr + size; ++ptr) {
+        sum += *ptr;
+    }
+
+    // 6. Display Sum
+    cout << "Sum of array elements: " << sum << endl;
+
+    // 7. Deallocation
+    delete[] arr;
 
     return 0;
 }
