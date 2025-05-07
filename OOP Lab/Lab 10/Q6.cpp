05162614b90d2b0f204ac4a519f3c09cf63833d3
Syed Huzaif <syedhuzaifalishah987@gmail.com>
2025-05-07 09:50:05 +0500

    Update and rename Q2.cpp to Q6.cpp

---
+++ OOP Lab/Lab 10/Q6.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+struct InventoryItem {
+    	int itemID;
+    	char itemName[20];
+};
+
+int main() {
+
+    	InventoryItem itemOut;
+    	itemOut.itemID = 101;
+    	strncpy(itemOut.itemName, "Screwdriver", sizeof(itemOut.itemName));
+    	itemOut.itemName[sizeof(itemOut.itemName) - 1] = '\0';
+
+    	ofstream outFile("inventory.dat", ios::binary);
+    	if (!outFile) {
+        	cerr << "Error: Could not open file for writing." << endl;
+        	return 1;
+    	}
+    	outFile.write(reinterpret_cast<char*>(&itemOut), sizeof(InventoryItem));
+    	outFile.close();
+
+    	InventoryItem itemIn;
+    	ifstream inFile("inventory.dat", ios::binary);
+    	if (!inFile) {
+        	cerr << "Error: Could not open file for reading." << endl;
+        	return 1;
+    	}
+    	inFile.read(reinterpret_cast<char*>(&itemIn), sizeof(InventoryItem));
+    	inFile.close();
+
+    	cout << "Loaded Item ID: " << itemIn.itemID << endl;
+    	cout << "Loaded Item Name: " << itemIn.itemName << endl;
+
+    	return 0;
+}
